bad05bd7b7baafc1313fa00a53c5fb147a23f313
joseph thomas <joedt100@protonmail.com>
2022-12-04 22:27:59 +0000

    added day 4

---
+++ 4_2.py
@@ -0,0 +1,8 @@
+with open("input.txt") as f:
+    pairs = f.read().split('\n')[:-1]
+num_duplicates = 0
+for pair in pairs:
+    elf1, elf2 = [[int(v) for v in p.split('-')] for p in pair.split(',')]
+    elf1, elf2 = range(elf1[0], elf1[1]+1), range(elf2[0], elf2[1]+1)
+    num_duplicates += int(set(elf1)-set(elf2)!=set(elf1))
+print(num_duplicates)
